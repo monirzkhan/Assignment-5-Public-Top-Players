f1c218b01ea36fc85416569b9c1d9b651652eb19
monirzkhan <odvutkabbo@gmail.com>
2022-08-22 12:30:54 +0600

    final expenses calculated

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -19,5 +19,49 @@ document.getElementById('calculate-button').addEventListener('click', function (
 
     const totalPlayersExpenses = expensePerPlayerInput * 5;
     playersExpenseTotal.innerText = totalPlayersExpenses;
-    return totalPlayersExpenses;
+    
+
+    // Total Budget Calculation
+    document.getElementById('total-calculate-button').addEventListener('click', function () {
+        const managerInputField = document.getElementById('manager-input-field');
+        const managerInputFieldString = managerInputField.value;
+        const managerInputFieldAmount = parseFloat(managerInputFieldString);
+        if (isNaN(managerInputFieldAmount)) {
+            alert('Please input and Number');
+            managerInputField.value = '';
+            return;
+        }
+        else if (managerInputFieldAmount < 0) {
+            alert('Please input a positive number');
+            managerInputField.value = '';
+            return;
+        }
+    
+        const coachInputField = document.getElementById('coach-input-field');
+        const coachInputFieldString = coachInputField.value;
+        const coachInputFieldAmount = parseFloat(coachInputFieldString);
+        if (isNaN(coachInputFieldAmount)) {
+            alert('Please input and Number');
+            coachInputField.value = '';
+            return;
+        }
+        else if (coachInputFieldAmount < 0) {
+            alert('Please input a positive number');
+            coachInputField.value = '';
+            return;
+        }
+    
+        const totalBudget = document.getElementById('expenses-total');
+        const totalBudgetString = totalBudget.innerText;
+        const totalBudgetAmount = parseFloat(totalBudgetString);
+    
+        // const expensePlayers = totalPlayersExpenses;
+    
+        const totalBudgets = managerInputFieldAmount + coachInputFieldAmount;
+        const finalBudget = totalPlayersExpenses + totalBudgets;
+        totalBudget.innerText = finalBudget;
+        // return totalBudgets;
+    })
 })
+
+
